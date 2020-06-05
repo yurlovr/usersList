41836e7f633a2ee43f634efb1a9935616486461d
Roman <r.yurlov@promo-bot.ru>
2020-06-05 22:36:01 +0500

    fix

--- src/store/controllers/appController.js
+++ src/store/controllers/appController.js
@@ -25,131 +25,131 @@ export default function () {
         }
       }
       switch (mutation.type) {
-            case 'app/SET_IS_LOADING':
-              if(!payload.data) return
-              switch(payload.data) {
-                case LOADING_ACTIONS.USERS:
-                  userInitialState()
-                  Api.Users({offset: getter['page/getOffset'],  limit: getter['page/getLimit']})
-                  .then(res => {
-                    const { recordsTotal, data} = res
-                    dispatch('app/setUsersData', {
-                      meta,
-                      data
-                    })
-                    dispatch('page/setTotalRecords', {
-                      meta,
-                      data: recordsTotal
-                    })
-                    dispatch('app/setIsLoading', {
-                      meta,
-                      data: null
-                    })
-                  })
-                  .catch(error => console.log(error))
-                  break
-                case LOADING_ACTIONS.USER:
-                  Api.GetCurrentUser({
-                    user_id: getter['app/getCurrentUserId']
-                  })
-                  .then(res => {
-                    setTimeout(() => {
-                      dispatch('app/setCurrentUser', {
-                        meta,
-                        data: res
-                      })
-                      dispatch('app/setIsLoading', {
-                        meta,
-                        data: null
-                      })
-                    }, REFRESH_USER_TIMEOUT)
-                  })
-                  .catch((error) => {
-                    console.log(error)
-                  })
-                  break
-                  case LOADING_ACTIONS.REPORT:
-                    Api.GetReport({ user_id: getter['app/getCurrentUserId'],
-                                      report: true,
-                                    ...getPeriodForRequest(getter['user/getPeriod'])})
-                    .then(res => {
-                      dispatch('user/setUserTransactions', {
-                        meta,
-                        data: res
-                      })
-                      dispatch('app/setIsLoading', {
-                        meta,
-                        data: null
-                      })
-                    })
-                    .catch(error => console.log(error))
-                    break
-                }
-            break
-            case 'app/SET_CREATE_NEW_USER':
-              Api.CreateUser({
-                user_id: uuid(),
-                user_name: getter['user/getUserName'].trim(),
-                user_custom: getter['user/getUserCustomName'].trim(),
-                email: getter['user/getUserEmail'].trim()
-              }).then(res => {
-                if (res.data.message) {
-                  message = res.data.message
-                }
-                dispatch('app/setUserCreatedMessage', {
+        case 'app/SET_IS_LOADING':
+          if(!payload.data) return
+          switch(payload.data) {
+            case LOADING_ACTIONS.USERS:
+              userInitialState()
+              Api.Users({offset: getter['page/getOffset'],  limit: getter['page/getLimit']})
+              .then(res => {
+                const { recordsTotal, data} = res
+                dispatch('app/setUsersData', {
                   meta,
-                  data: message
+                  data
+                })
+                dispatch('page/setTotalRecords', {
+                  meta,
+                  data: recordsTotal
+                })
+                dispatch('app/setIsLoading', {
+                  meta,
+                  data: null
                 })
               })
-              .catch(() => {
-                dispatch('app/setUserCreatedMessage', {
-                  meta,
-                  data: SMTH_GO_WRONG
-                })
-              })
+              .catch(error => console.log(error))
               break
-            case 'app/SET_USER_CREATED_MESSAGE':
-              if (payload.data) {
+            case LOADING_ACTIONS.USER:
+              Api.GetCurrentUser({
+                user_id: getter['app/getCurrentUserId']
+              })
+              .then(res => {
                 setTimeout(() => {
-                  dispatch('app/setUserCreatedMessage', {
-                    meta,
-                    data: null
-                  })
-                  dispatch('user/setInitialState', {
-                    meta
-                  })
-                  dispatch('modal/setCreateUser', {
-                    meta,
-                    data: false
-                  })
-                  dispatch('app/setIsLoading', {
-                    meta,
-                    data: LOADING_ACTIONS.USERS
-                  })
-                }, CREATED_TIMEOUT)
-              }
-              break
-              case 'app/SET_CURRENT_USER_ID':
-                if (!payload.data) {
-                  dispatch('app/setShowBtnCreateUser', {
-                    meta,
-                    data: HEADER_BUTTON_STATE.SHOW
-                  })
                   dispatch('app/setCurrentUser', {
                     meta,
-                    data: null
-                  })
-                } else {
-                  dispatch('app/setShowBtnCreateUser', {
-                    meta,
-                    data: HEADER_BUTTON_STATE.HIDE
+                    data: res
                   })
                   dispatch('app/setIsLoading', {
                     meta,
-                    data: LOADING_ACTIONS.USER
+                    data: null
                   })
-                }
+                }, REFRESH_USER_TIMEOUT)
+              })
+              .catch((error) => {
+                console.log(error)
+              })
+              break
+              case LOADING_ACTIONS.REPORT:
+                Api.GetReport({ user_id: getter['app/getCurrentUserId'],
+                                  report: true,
+                                ...getPeriodForRequest(getter['user/getPeriod'])})
+                .then(res => {
+                  dispatch('user/setUserTransactions', {
+                    meta,
+                    data: res
+                  })
+                  dispatch('app/setIsLoading', {
+                    meta,
+                    data: null
+                  })
+                })
+                .catch(error => console.log(error))
                 break
+            }
+          break
+        case 'app/SET_CREATE_NEW_USER':
+          Api.CreateUser({
+            user_id: uuid(),
+            user_name: getter['user/getUserName'].trim(),
+            user_custom: getter['user/getUserCustomName'].trim(),
+            email: getter['user/getUserEmail'].trim()
+          }).then(res => {
+            if (res.data.message) {
+              message = res.data.message
+            }
+            dispatch('app/setUserCreatedMessage', {
+              meta,
+              data: message
+            })
+          })
+          .catch(() => {
+            dispatch('app/setUserCreatedMessage', {
+              meta,
+              data: SMTH_GO_WRONG
+            })
+          })
+          break
+        case 'app/SET_USER_CREATED_MESSAGE':
+          if (payload.data) {
+            setTimeout(() => {
+              dispatch('app/setUserCreatedMessage', {
+                meta,
+                data: null
+              })
+              dispatch('user/setInitialState', {
+                meta
+              })
+              dispatch('modal/setCreateUser', {
+                meta,
+                data: false
+              })
+              dispatch('app/setIsLoading', {
+                meta,
+                data: LOADING_ACTIONS.USERS
+              })
+            }, CREATED_TIMEOUT)
+          }
+          break
+        case 'app/SET_CURRENT_USER_ID':
+          if (!payload.data) {
+            dispatch('app/setShowBtnCreateUser', {
+              meta,
+              data: HEADER_BUTTON_STATE.SHOW
+            })
+            dispatch('app/setCurrentUser', {
+              meta,
+              data: null
+            })
+          } else {
+            dispatch('app/setShowBtnCreateUser', {
+              meta,
+              data: HEADER_BUTTON_STATE.HIDE
+            })
+            dispatch('app/setIsLoading', {
+              meta,
+              data: LOADING_ACTIONS.USER
+            })
+          }
+          break
       }
     })
   }
