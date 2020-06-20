34861f84577d44ad346fc415e9e0b1e943c60a3d
Roman <r.yurlov@promo-bot.ru>
2020-06-20 17:23:00 +0500

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  publicPath: (process.env.NODE_ENV === 'development') ? '' : '/usersList/',
-}
+// module.exports = {
+//   publicPath: (process.env.NODE_ENV === 'development') ? '' : '/usersList/',
+// }
